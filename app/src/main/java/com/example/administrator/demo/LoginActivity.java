f2e869900a9260a911b95adcfdde21213b4500a9
zeng <547155474@qq.com>
2017-10-17 20:18:07 +0800

    setButton

--- app/src/main/java/com/example/administrator/demo/LoginActivity.java
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.administrator.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private String username;
+    private String userpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final TextView usernameview = (TextView) findViewById(R.id.login_username);
+        final TextView passwordview = (TextView) findViewById(R.id.login_password);
+        Button login=(Button)findViewById(R.id.btn_login);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
