8cd1eecd7e1c14bae22596ca448aadb81fb408a3
zeng <547155474@qq.com>
2017-10-19 21:36:49 +0800

    实现登陆需要正确用户和密码

--- app/src/main/java/com/example/administrator/demo/LoginActivity.java
+++ app/src/main/java/com/example/administrator/demo/LoginActivity.java
@@ -7,25 +7,44 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-    private String username;
-    private String userpass;
+public class LoginActivity extends AppCompatActivity{
+    private String username="zmj";
+    private String userpass="123";
+    private String usernameInfo;
+    private String passwordInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
         final TextView usernameview = (TextView) findViewById(R.id.login_username);
+        usernameview.setText(usernameInfo);
         final TextView passwordview = (TextView) findViewById(R.id.login_password);
+        passwordview.setText(passwordInfo);
         Button login=(Button)findViewById(R.id.btn_login);
-        login.setOnClickListener(this);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                String inputname= usernameview.getText().toString().trim();
+                String inputpassword=passwordview.getText().toString().trim();
+                if (inputname.isEmpty() || inputpassword.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "用户名和密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (inputname.equals(username) && inputpassword.equals(userpass)) {
+                      Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                      startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "用户名或密码有误", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-        startActivity(intent);
-    }
+
 }
