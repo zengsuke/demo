7a3ac6cc3cb7602b722aba19962d358808fb6736
ASUS <547155474@qq.com>
2017-11-02 21:34:42 +0800

    grpc

--- app/src/main/java/com/example/administrator/demo/Activity/LoginActivity.java
+++ app/src/main/java/com/example/administrator/demo/Activity/LoginActivity.java
@@ -12,6 +12,18 @@ import android.widget.Toast;
 
 import com.example.administrator.demo.Class.CircularAnim;
 import com.example.administrator.demo.R;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.LoginReply;
+import io.grpc.examples.helloworld.LoginRequest;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static io.grpc.okhttp.internal.Platform.logger;
 
 
 public class LoginActivity extends AppCompatActivity{
@@ -24,6 +36,8 @@ public class LoginActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+
         final TextView usernameview = (TextView) findViewById(R.id.login_username);
         final TextView passwordview = (TextView) findViewById(R.id.login_password);
         final ProgressBar mProgressBar=(ProgressBar)findViewById(R.id.ProgressBar);
@@ -33,11 +47,12 @@ public class LoginActivity extends AppCompatActivity{
 
                 String inputname= usernameview.getText().toString().trim();
                 String inputpassword=passwordview.getText().toString().trim();
+                Boolean login = HelloWorldClient("10.0.2.2",50051,inputname,inputpassword);
                 if (inputname.isEmpty() || inputpassword.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "用户名和密码不能为空", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (inputname.equals(username) && inputpassword.equals(userpass)) {
+                if (login) {
                     CircularAnim.fullActivity(LoginActivity.this, mProgressBar)
                             .colorOrImageRes(R.color.colorPrimary)
                             .go(new CircularAnim.OnAnimationEndListener() {
@@ -56,5 +71,43 @@ public class LoginActivity extends AppCompatActivity{
         });
     }
 
+        private ManagedChannel channel;
+        private GreeterGrpc.GreeterBlockingStub blockingStub;
 
+        /** Construct client connecting to HelloWorld server at {@code host:port}. */
+        public Boolean HelloWorldClient(String host, int port,String name,String password) {
+
+            try {
+                channel = ManagedChannelBuilder.forAddress(host, port)
+                        .usePlaintext(true)
+                        .build();
+                blockingStub = GreeterGrpc.newBlockingStub(channel);
+                LoginRequest request = LoginRequest.newBuilder().setName(name).setPassword(password).build();
+                LoginReply response = blockingStub.logon(request);
+                System.out.println(response.getMe()+"fuck u");
+                if(response.getMe().equals("yes")){
+                    return true;
+                }else {
+                    return false;
+                }
+            }catch (Exception e){
+                return false;
+            }
+
+        }
+
+        public void shutdown() throws InterruptedException {
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        }
+
+        /** Say hello to server. */
+
+
+        /**
+         * Greet server. If provided, the first element of {@code args} is the name to use in the
+         * greeting.
+         */
 }
+
+
+
